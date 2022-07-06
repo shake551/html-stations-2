e3bb34ce97c91420abfe4c5c2437bdf3c5205412
shake <hiroki.yamako@ezweb.ne.jp>
2022-07-06 20:32:50 +0900

    change color

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,13 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const is_checked = document.getElementById("check").checked
+
+  console.log(is_checked)
+
+  if (is_checked) {
+    document.getElementById('text').style.backgroundColor = 'red'
+  } else {
+    document.getElementById('text').style.backgroundColor = 'white'
+  }
 
 }
